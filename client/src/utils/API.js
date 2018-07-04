f193db504d86bf7699f35062f0ce02f7afdf77da
Bobby-Lishez <right2bob@msn.com>
2018-07-04 11:19:25 -0600

    added API

---
+++ client/src/utils/API.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const APIKEY = 'b9f91d369ff59547cd47b931d8cbc56b:0:74623931';
+
+const queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + APIKEY + '&q=';
+
+export default {
+  //searches new articles from ny times
+  scrape: function(query) {
+    return axios.get(`${queryURL}${query}`);
+  }, 
+  // Gets all saved articles
+  getSaved: function() {
+    return axios.get("/api/saved");
+  },
+  // Gets the book with the given id
+//   getBook: function(id) {
+//     return axios.get("/api/books/" + id);
+//   },
+  // Deletes the article with the given id
+  deleteArticle: function(id) {
+    return axios.delete("/api/saved/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(article) {
+    return axios.post("/api/saved", article);
+  }
+};
